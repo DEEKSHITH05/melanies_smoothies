978086e76ffade97ddc10583ab35b39e06fc5bec
Deekshith Swamy S M G <143374165+DEEKSHITH05@users.noreply.github.com>
2025-10-14 19:22:31 +0200

    Update streamlit_app.py
    Added changes to make it work

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
-# Write directly to the app
+# Write directly to the app :
 st.title(f":cup_with_straw: Custom Smoothie Order Form :apple:")
 st.write(
   """Choose the fruits that you want in your Custom Smootie !
